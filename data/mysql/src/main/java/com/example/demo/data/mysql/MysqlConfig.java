39f7ce05c8debcf168e8b929f6035a337a0f1d6a
belljun3395 <195850@jnu.ac.kr>
2023-08-08 13:43:34 +0900

    SKEL-001 feat: MysqlConfig 파일에 공통 상수 추가

--- data/mysql/src/main/java/com/example/demo/data/mysql/MysqlConfig.java
+++ data/mysql/src/main/java/com/example/demo/data/mysql/MysqlConfig.java
@@ -17,4 +17,8 @@ import org.springframework.context.annotation.Configuration;
 		})
 public class MysqlConfig {
 	public static final String BASE_PACKAGE = "com.example.demo.data.mysql";
+	public static final String SERVICE_NAME = "demo";
+	public static final String MODULE_NAME = "mysql";
+	public static final String BEAN_NAME_PREFIX = SERVICE_NAME + MODULE_NAME;
+	public static final String PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME;
 }
