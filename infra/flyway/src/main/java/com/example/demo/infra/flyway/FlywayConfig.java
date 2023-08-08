b70aaa8996d366a64d633deb806a1d2b956c5e44
belljun3395 <195850@jnu.ac.kr>
2023-08-08 13:43:43 +0900

    SKEL-001 feat: FlywayConfig 파일에 공통 상수 추가

--- infra/flyway/src/main/java/com/example/demo/infra/flyway/FlywayConfig.java
+++ infra/flyway/src/main/java/com/example/demo/infra/flyway/FlywayConfig.java
@@ -7,4 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = FlywayConfig.BASE_PACKAGE)
 public class FlywayConfig {
 	public static final String BASE_PACKAGE = "com.example.demo.infra.flyway";
+	public static final String SERVICE_NAME = "demo";
+	public static final String MODULE_NAME = "flyway";
+	public static final String BEAN_NAME_PREFIX = SERVICE_NAME + MODULE_NAME;
+	public static final String PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME;
 }
